defb1b6c87251474a1be7c3b91692e95d99ee5a5
Amami Ruri <babichan@outlook.com>
2021-12-23 13:14:49 +0800

    离散数学

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const A = true;
+const B = false;
+// 逻辑非
+console.log(!A);
 
+// 逻辑与
+console.log(A && B);
+
+// 逻辑或
+console.log(A || B);
+
+// 逻辑异或
+console.log(Boolean(A ^ B));
+
+// 非A或B(蕴含)
+console.log(Boolean(A ? B : true));
